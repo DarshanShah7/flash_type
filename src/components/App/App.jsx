082a37df8273feb3402a0da4b7f05a3c7a9deb74
darshanshah-dev <darshanshah3010@gmail.com>
2021-12-19 10:51:51 +0530

    navbar component created

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Nav from '../Nav/Nav';
+function App() {
+  return (
+    <div className="app">
+            {/* navbar */}
+          <Nav/>
+          
+    </div>
+  );
+}
+
+export default App;
