f6cf592be43fa1080a98137881e875f4089a16a3
darshanshah-dev <darshanshah3010@gmail.com>
2021-12-19 12:26:53 +0530

    Landing component with animation and typewriting effect added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Nav from '../Nav/Nav';
+import Landing from '../Landing/Landing';
 function App() {
   return (
     <div className="app">
             {/* navbar */}
           <Nav/>
-          
+          <Landing/>
+
     </div>
   );
 }
